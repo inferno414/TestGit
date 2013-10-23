c540c83329788c5597d3971124c1480bf9fe9cba
inferno414 <sacredilluzion@gmail.com>
2013-10-24 01:16:14 +0400

    другая ветка

--- Testgit/src/figure/Test.java
+++ Testgit/src/figure/Test.java
@@ -3,10 +3,11 @@ package figure;
 public class Test {
 
 	public static void main(String[] args) {
-		Figure arr[] = new Figure[2];
+		Figure arr[] = new Figure[3];
 		arr[0] = new Triangle(new Point(1.4,1.), new Point(20.4,3.6),new Point(7.5,30.6));
 		arr[1] = new Circle(new Point(3.,3.),10);
-		for(int i=0; i<2;i++) {
+		arr[2] = new Square(new Point(1.,2.),5.3);
+		for(int i=0; i<3;i++) {
 			System.out.println("Area "+ i + " = " + arr[i].GetArea());
 		}
 		

e643b82deab7a9b5330e545e6890dca461d72851
inferno414 <sacredilluzion@gmail.com>
2013-10-26 00:09:19 +0400

    еще гуй

--- Testgit/src/otherpack/MyJFrame.java
+++ Testgit/src/otherpack/MyJFrame.java
@@ -1,12 +1,36 @@
 package otherpack;
 
+
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
 import figure.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
-public class MyJFrame extends javax.swing.JFrame {
-
-	/**
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class MyJFrame extends javax.swing.JFrame implements ActionListener{
+	private static final long serialVersionUID = 1L;
+	private JButton jButton1;
+	private JLabel jLabel1;
+	private JButton jButton3;
+	private JButton jButton2;
+/**
 	* Auto-generated main method to display this JFrame
 	*/
 	public static void main(String[] args) {
@@ -26,13 +50,57 @@ public class MyJFrame extends javax.swing.JFrame {
 	
 	private void initGUI() {
 		try {
+			GridBagLayout thisLayout = new GridBagLayout();
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			thisLayout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
+			thisLayout.rowHeights = new int[] {7, 7, 7, 7};
+			thisLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1};
+			thisLayout.columnWidths = new int[] {7, 7, 7, 7};
+			getContentPane().setLayout(thisLayout);
+
+			{
+				jButton1 = new JButton();
+				getContentPane().add(jButton1, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+				jButton1.setText("close");
+				jButton1.addActionListener(this);
+				
+			}
+			{
+				jButton2 = new JButton();
+				getContentPane().add(jButton2, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+				jButton2.setText("jButton2");
+				jButton2.setPreferredSize(new java.awt.Dimension(474, 375));
+				jButton2.addActionListener(this);
+			}
+			{
+				jButton3 = new JButton();
+				getContentPane().add(jButton3, new GridBagConstraints(3, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+				jButton3.setText("jButton3");
+			}
+			{
+				jLabel1 = new JLabel();
+				getContentPane().add(jLabel1, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+				jLabel1.setText("");
+			}
+
 			pack();
-			setSize(400, 300);
+			this.setSize(663, 415);
 		} catch (Exception e) {
 		    //add your error handling code here
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//System.out.println(e.getActionCommand());
+		if(e.getSource().equals(jButton1)){
+			dispose();
+		}
+		if(e.getSource().equals(jButton2)){
+			jLabel1.setText(String.valueOf(e.getID()));
+		}
+		
 	}
 
 }
